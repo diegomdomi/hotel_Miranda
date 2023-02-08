4a3c92f4ee46b65630db81df0bd27cf1f6fda78e
diegomdomi <diegomdomi@gmail.com>
2023-02-08 14:40:04 +0100

    js in navbar mobile

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable:true
     },
   
     // Navigation arrows
@@ -17,5 +18,25 @@ const swiper = new Swiper('.swiper', {
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
+      draggable:true,
     },
   });
+
+const menuMobil = document.querySelector('.container-menu');
+const menuOpenMobil = menuMobil.querySelector('.container-menu__burger')
+const menuCloseMobil = document.querySelector('.container-menu__burger-close');
+menuCloseMobil.style.display="none";
+
+function toggleMenu(e) {
+  e.preventDefault();
+  let toggleMenuMobil =  document.querySelector( ".nav__list" ).classList.toggle("is_active");
+  if(toggleMenuMobil){
+    menuOpenMobil.style.display="none"
+    menuCloseMobil.style.display="inline-block"
+  }else{
+    menuOpenMobil.style.display="initial"
+    menuCloseMobil.style.display="none"
+
+  }
+}
+menuMobil.addEventListener('click', toggleMenu);
