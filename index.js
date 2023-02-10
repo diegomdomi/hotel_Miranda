a4b74601fafd1e68830fcf99c24147d004d7874d
diegomdomi <diegomdomi@gmail.com>
2023-02-11 00:45:46 +0100

    fixing slider in index.html

--- index.js
+++ index.js
@@ -22,6 +22,83 @@ const swiper = new Swiper('.swiper', {
     },
   });
 
+
+  const swiper_facilities = new Swiper('.swiper_facilities', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper_facilities-button-next',
+      prevEl: '.swiper_facilities-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper_facilities-scrollbar',
+      draggable:true,
+    },
+  });
+
+
+  const swiper_food = new Swiper('.swiper_food', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper_food-button-next',
+      prevEl: '.swiper_food-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper_food-scrollbar',
+      draggable:true,
+    },
+  });
+
+  const swiper_gourmet = new Swiper('.gourmet', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-gourmet-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper_food-button-next',
+    //   prevEl: '.swiper_food-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper_gourmet-scrollbar',
+      draggable:true,
+    },
+  });
+
+
+
+
 const menuMobil = document.querySelector('.container-menu__toggle');
 const menuOpenMobil = menuMobil.querySelector('.container-menu__burger')
 const menuCloseMobil = document.querySelector('.container-menu__burger-close');
